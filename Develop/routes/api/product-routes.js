e40e7855240accbe801a889a70c99190264c02ed
Jose Plasencia <josejpd3@gmail.com>
2022-11-08 17:36:08 -0500

    fixed syntax error

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({ include: [{model: Tag, through: ProductTag,}]});
+    const productData = await Product.findAll({ include: [{model: Tag, through: ProductTag,}, {model: Category,}]});
     res.json({ productData })
   } catch (error) {
     res.status(500).json(err);
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id, {include: [{model: Tag, through: ProductTag,}]})
+    const productData = await Product.findByPk(req.params.id, {include: [{model: Tag, through: ProductTag,}, {model: Category,}]})
     if (!productData) {
       res.status(404).json({message: 'No product matches this id.'})
     }
