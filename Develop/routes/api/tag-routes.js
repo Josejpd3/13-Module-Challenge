cd73befd75791caa88e619bb0813f2e56f3c7b24
Jose Plasencia <josejpd3@gmail.com>
2022-11-08 18:50:46 -0500

    update tag name by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -41,16 +41,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updatedTag = await Tag.update(
-      {
-        tag_name: req.body.tag_name,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      },
-    );
+    const updatedTag = await Tag.update({tag_name: req.body.tag_name,},{where: {id: req.params.id,},},);
     res.status(200).json(updatedTag);
   } catch (err) {
     res.status(500).json(err);
